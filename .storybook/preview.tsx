be6beac000bdafa3e43b3640fc099c8fb3c695aa
Matt Jonas <matt.jonas@gmail.com>
2023-05-22 17:58:23 -0700

    Fix Storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,15 +3,26 @@ import React from 'react';
 import { Preview } from "@storybook/react";
 import { DefaultTheme, ThemeProvider } from "styled-components";
 
-import constants from "../src/lib/design_tokens/constants";
+import { 
+  useBorderTokens,
+  useFontTokens,
+  useSpacingTokens,
+  useColorTokens,
+} from '../src/lib';
 
 const preview: Preview = {
   decorators: [
     (Story) => {
-      const theme: DefaultTheme = {
-        mode: "light",
-        constants,
+      const borderTokens = useBorderTokens();
+      const fontTokens = useFontTokens();
+      const spacingTokens = useSpacingTokens();
+      const colorTokens = useColorTokens('violet', 'light');
+      const constants = {
+        ...borderTokens,
+        ...fontTokens,
+        ...spacingTokens,
       };
+      const theme: DefaultTheme = { constants, colors: colorTokens };
       
       return (
         <ThemeProvider theme={theme}>
