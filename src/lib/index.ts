f293199864acd83c18b4cc736def2b89783f39ee
Matt Jonas <matt.jonas@gmail.com>
2023-08-14 17:40:53 -0700

    Add types to export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,12 @@ export type { default as BorderTokens } from './types/BorderTokens'
 export type { default as ColorTokens } from './types/ColorTokens'
 export type { default as FontTokens } from './types/FontTokens'
 export type { default as SpacingTokens } from './types/SpacingTokens'
+export type { default as Palette } from './types/Palette'
+export type { default as PaletteBoard } from './types/PaletteBoard'
+export type {
+	default as PaletteOption,
+	TrainualBrandPaletteOption
+} from './types/PaletteOption'
 
 // Hooks
 export { default as useBorderTokens } from './hooks/useBorderTokens'
@@ -10,7 +16,6 @@ export { default as useColorTokens } from './hooks/useColorTokens'
 export { default as useFontTokens } from './hooks/useFontTokens'
 export { default as useHeightTokens } from './hooks/useHeightTokens'
 export { default as useSpacingTokens } from './hooks/useSpacingTokens'
-
 
 // Styled Components Integrations
 export type { default as SaguaroTheme } from './types/SaguaroTheme'
