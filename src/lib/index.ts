a7bdc880efb467ff6e01fbd5f00acd4793d424f1
Jared Gray <jared@jaredgray.us>
2024-07-29 08:32:20 -0700

    Expose LimitedPalette and ExpandedPalette
    Although when using export as default for the types/Palette directory, was not able
    to use within the Trainual app as with usage:
    
    import { ExpandedPalette } from 'saguaro';
    
    Was getting the following error:
    TS2305: Module "saguaro" has no exported member ExpandedPalette

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,7 +3,7 @@ export type { default as BorderTokens } from './types/BorderTokens'
 export type { default as ColorTokens } from './types/ColorTokens'
 export type { default as FontTokens } from './types/FontTokens'
 export type { default as SpacingTokens } from './types/SpacingTokens'
-export type { default as Palette } from './types/Palette'
+export type { LimitedPalette, ExpandedPalette  } from './types/Palette'
 export type { default as PaletteBoard } from './types/PaletteBoard'
 export type {
 	default as PaletteOption,
