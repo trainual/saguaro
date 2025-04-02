9eb5a91cdf15568b05a12e93685b5105612d0ae9
DistortionJim <itpenergy@gmail.com>
2025-04-02 15:01:26 +0300

    Add new `contrastTextForAccentPrimary` color to the ThemePreview

--- src/lib/design_tokens/preview/ThemePreview/ThemePreview.tsx
+++ src/lib/design_tokens/preview/ThemePreview/ThemePreview.tsx
@@ -59,6 +59,7 @@ const ThemePreview = () => {
         <ColorRow hex={vars.accentSubdued4} text="accent-subdued-4" />
         <ColorRow hex={vars.accentStrong1} text="accent-strong-1" />
         <ColorRow hex={vars.accentStrong2} text="accent-strong-2" />
+        <ColorRow hex={vars.contrastTextForAccentPrimary} text="contrast-text-for-accent-primary" />
       </StyleGroupBody>
 
       <StyleGroupHeader title="State" description="Critical UI States" />
